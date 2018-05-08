6eb6343ebe741d2b4ab454c6b592b390e9451601
David4FSU <david4fsu@hotmail.com>
2018-05-08 10:20:43 -0500

    very basic flipping in place

--- js/app.js
+++ js/app.js
@@ -1,14 +1,8 @@
-/*
- * Create a list that holds all of your cards
- */
+$(document).ready(function () {
+    startNewGame();
+});
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -26,19 +20,13 @@ function shuffle(array) {
     return array;
 }
 
+function startNewGame() {
+    // This function is called whenever the page is loaded, 
+    // or the refresh button is pressed
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-$(document).ready(function () {
+    /*
+     * Create a list that holds all of your cards
+     */
     let myDeck = shuffle(["fa fa-diamond",
         "fa fa-paper-plane-o",
         "fa fa-anchor",
@@ -57,18 +45,21 @@ $(document).ready(function () {
         "fa fa-cube"
     ]);
 
-   
+    /*
+     * Display the cards on the page
+     *   - shuffle the list of cards using the provided "shuffle" method below
+     *   - loop through each card and create its HTML
+     *   - add each card's HTML to the page
+     */
     let myCounter = 1;
     myDeck.forEach(function (element) {
         let myLocationID = "#" + "location" + myCounter.toString();
-        
+
         //Temp to show all values
         let myParent = $(myLocationID).parent();
         $(myParent).removeClass();
-        $(myParent).addClass("card open show");
+        $(myParent).addClass("card");
 
-
-        console.log(myLocationID + " = " + element);
         $(myLocationID).removeClass();
         $(myLocationID).addClass(element);
 
@@ -76,9 +67,33 @@ $(document).ready(function () {
     })
 
 
-    $(".card").click(function () {
-        console.log("Clicked");
-    });
 
 
+}
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+$(".card").click(function () {
+    console.log("Clicked " + this.className);
+
+    if (this.className == "card open show") {
+        this.className = "card";
+    }
+    else if (this.className == "card") {
+        this.className = "card open show";
+    }
+});
+
+// Handle the restart button to reinitialize the game board
+$(".restart").click(function () {
+    startNewGame();
 });
