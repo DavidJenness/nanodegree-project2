29011ef0eab6de500af9db02bd54c2f08d77e4aa
David4FSU <david4fsu@hotmail.com>
2018-05-08 08:21:21 -0500

    basic jQuery click wiring

--- js/app.js
+++ js/app.js
@@ -36,3 +36,15 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+$( document ).ready(function() {
+  console.log("ready player one");
+   
+  $(".card").click(function() {
+        console.log("Clicked");
+    });
+
+
+  });
+
+
